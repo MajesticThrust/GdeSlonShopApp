df83deb74b262630aecad85fcbbfcbaca7ea973b
MajesticThrust <majesticthrust@gmail.com>
2020-07-20 03:47:07 +0500

    Basic account page fix

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/auth.service";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.page.html',
-  styleUrls: ['./account.page.scss'],
+  selector: "app-account",
+  templateUrl: "./account.page.html",
+  styleUrls: ["./account.page.scss"],
 })
 export class AccountPage implements OnInit {
+  public user = this.authService.currentUser;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
